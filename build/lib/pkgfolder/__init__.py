7724d81c8db252e899ce86988fda3c6a2a54bc51
arash <arash@email.com>
2023-12-04 06:20:58 -0800

    init commit

---
+++ build/lib/pkgfolder/__init__.py
@@ -0,0 +1,1 @@
+from pkgfolder.pkgfile import test
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(
+    name='arashpkg',
+    version='1.0',
+    author='arash',
+    packages=['pkgfolder'],
+)
